a768a957890cb8784e98a835c02d90cb3d1379f3
nktx <nkt.unknown@gmail.com>
2018-08-26 22:32:30 +0800

    animate intro page

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      view: 'start',
+      view: 'intro',
       toggleQuestionSets: 0,
       playerScore: 0,
       playerAnswers: [],
@@ -157,15 +157,51 @@ class Start extends Component {
 }
 
 class Intro extends Component {
+  componentDidMount() {
+    let tlButtonMove = new TimelineMax({
+      repeat: -1
+    });
+
+    tlButtonMove
+      .to(this.btn, .25, {scale: 0.96, ease: Elastic.easeIn})
+      .from(this.btn, .75, {scale: 0.96, ease: Elastic.easeOut});
+
+    let tlAvatarShake = new TimelineMax({
+      paused: true,
+      repeat: -1,
+      yoyo: true
+    });
+
+    tlAvatarShake
+      .to(this.avatar, .5, { x: '-2px', y: '1px', rotation: -1, transformOrigin: "center bottom", ease: Power1.easeInOut })
+
+    let tlAvatarMove = new TimelineMax();
+
+    tlAvatarMove
+      .from(this.avatar, .5, { x: '80px', y: '480px', rotation: 4, transformOrigin: "right bottom", ease: Power1.easeIn })
+      .call(() => tlAvatarShake.play(), null, null, .5)
+      .fromTo(this.dialog, .5, { autoAlpha: 0, scale: 0, ease: Elastic.easeIn }, { autoAlpha: 1, scale: 1, ease: Elastic.easeIn })
+      .call(() => tlDialogShake.play(), null, null, 1);
+
+    let tlDialogShake = new TimelineMax({
+      paused: true,
+      repeat: -1,
+      yoyo: true
+    });
+  
+    tlDialogShake
+      .to(this.dialog, .5, { scale: '1.04', ease: Power1.easeInOut })
+  }
+
   render() {
     return (
       <div className="intro">
         <div className="intro-container">
           <div className="intro-title">遊戲介紹標題</div>
           <div className="intro-description">遊戲內容簡介遊戲內容簡介遊戲內容共二十字</div>
-          <div className="action-btn intro-btn" onClick={() => this.props.switchView('quiz')}>開始！</div>
-          <object className="svg svg-intro-avatar" data={svgImages['intro/avatar.svg']} type="image/svg+xml"> </object>
-          <object className="svg svg-intro-dialog" data={svgImages['intro/dialog.svg']} type="image/svg+xml"> </object>
+          <div className="action-btn intro-btn" onClick={() => this.props.switchView('quiz')} ref={(el) => {this.btn = el}}>開始！</div>
+          <object className="svg svg-intro-avatar" data={svgImages['intro/avatar.svg']} type="image/svg+xml" ref={(el) => {this.avatar = el}}> </object>
+          <object className="svg svg-intro-dialog" data={svgImages['intro/dialog.svg']} type="image/svg+xml" ref={(el) => {this.dialog = el}}> </object>
         </div>
       </div>
     );
