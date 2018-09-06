c1050cb3a1f0a1013565d1a6b31b5a1d67f90972
nktx <nkt.unknown@gmail.com>
2018-09-07 00:02:31 +0800

    add result description

--- src/Result.js
+++ src/Result.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { TimelineMax, Power1, Elastic } from 'gsap/all';
+import resultDescriptions from './result-descriptions';
 import svgImages from './svg-import.js';
 
 // radial-gradient(circle at center, #FFFFFF 0%, #FFFFFF 100%, #CDDC4C 70%, #CDDC4C 100%)
 
 let mock = {};
-mock.playerScore = 5;
+mock.playerScore = 0;
 mock.playerAnswers = [
   {questionId: 1, isPlayerAnswerCorrect: true},
   {questionId: 2, isPlayerAnswerCorrect: false},
@@ -25,6 +26,7 @@ class Result extends Component {
 
   componentDidMount() {
     let resultIllustration = this.result.querySelectorAll('.result-illustration');
+    let resultDescription = this.result.querySelectorAll('.result-description');
     let tlshowResult = new TimelineMax();
     tlshowResult
       .to(this.focus, .5, {scale: 1.2, ease: Elastic.easeIn})
@@ -35,6 +37,7 @@ class Result extends Component {
       .to(this.title, .25, {y: -480, ease: Power1.easeInOut}, "-=.25")
       .to(this.details, .25, {y: -500, ease: Power1.easeInOut}, "-=.25")
       .to(resultIllustration, .5, {opacity: 1, scale: 1, ease: Elastic.easeInOut})
+      .to(resultDescription, .25, {opacity: 1, ease: Power1.easeInOut})
       .to(this.actionBtn, .25, {opacity: 1, ease: Power1.easeInOut}, "-=.25");
   }
 
@@ -71,6 +74,7 @@ class Result extends Component {
           })}
         </div>
         <ResultIllustration playerScore={mock.playerScore} />
+        <ResultDescription playerScore={mock.playerScore} />
         <div className="action-btn intro-btn result-btn" onClick={() => this.props.switchView('start')} ref={(el) => {this.actionBtn = el}}>再玩一次！</div>
       </div>
     );
@@ -180,6 +184,33 @@ function ExcellentIllustraionSet(props) {
       <object className="svg svg-result-excellent-avatar" data={svgImages['result/excellent/avatar.svg']} type="image/svg+xml"> </object>
       <object className="svg svg-result-ribbon" data={svgImages['result/excellent/ribbon.svg']} type="image/svg+xml"> </object>
       <object className="svg svg-result-medal" data={svgImages['result/excellent/medal.svg']} type="image/svg+xml"> </object>
+    </div>
+  );
+}
+
+function ResultDescription(props) {
+  let resultDescription;
+
+  switch (props.playerScore) {
+    case 0:
+      resultDescription = resultDescriptions.okay;
+      break;
+    case 1:
+    case 2:
+      resultDescription = resultDescriptions.nice;
+      break;
+    case 3:
+    case 4:
+      resultDescription = resultDescriptions.great;
+      break;
+    case 5:
+      resultDescription = resultDescriptions.excellent;
+      break;
+  }
+
+  return (
+    <div className="result-description">
+      {resultDescription}
     </div>
   );
 }
---
+++ src/result-descriptions.js
@@ -0,0 +1,8 @@
+const resultDescriptions = {
+  okay:       "沒關係，先把牆上的知識\n仔細看過一遍，再回來重新挑戰！",
+  nice:       "有潛力～再仔細了解更多\n野外調查知識，你會表現更棒喔！",
+  great:      "很棒喔！再加把勁研究一番，\n你一定會成為優秀的調查家！",
+  excellent:  "你就是野外調查小專家！\n快教教其他人野外調查是什麼吧！"
+};
+
+export default resultDescriptions;
